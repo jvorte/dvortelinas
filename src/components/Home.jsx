6d8657873e911a926f2136f3ef381b35a3c77370
Dimitris <jvortelinas@gmail.com>
2025-02-12 15:22:58 +0100

    last

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -30,7 +30,7 @@ const Home = () => {
              to="about"
              smooth
              duration={500}
-             className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+             className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-amber-500 to-amber-500 cursor-pointer"
            >
              {t('homeButton')}
              <span className="group-hover:rotate-90 duration-300">
